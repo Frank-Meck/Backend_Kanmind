fea4955327d086a38c66f99e3552e46397f36950
Frank-Meck <frankmeckel@ymail.com>
2026-06-08 08:11:47 +0200

    Superuser added

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,5 +1,24 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -9,6 +28,8 @@ class User(AbstractUser):
 
     fullname = models.CharField(max_length=255, verbose_name="Full Name",)
     email = models.EmailField(unique=True, verbose_name="Email Address",)
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
